c0b5c34973abc16e3af4892359995e3fc0fe6875
Schirmchens <chiara.schirmer@student.hpi.de>
2020-11-05 09:32:27 +0100

    data patient liste

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ import './qrCode.dart';
 //import 'package:qr_code_scanner/qr_code_scanner.dart';
 //import 'package:relative_scale/relative_scale.dart';
 
+final double smallFontSize = 10;
+final double normalFontSize = 14;
+final double bigFontSize = 18;
+
 void main() {
   runApp(MyApp());
 }
@@ -35,60 +39,88 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final double smallFontSize = 10;
-    final double normalFontSize = 14;
-    final double bigFontSize = 18;
-
     Color containerColor = Theme.of(context).primaryColor;
     Color grayContainerColor = Colors.grey;
     Color fontColor = Colors.black;
 
     void _openDetailedInformation() {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => DetailedInformationPage(
-        smallFontSize: smallFontSize,
-        normalFontSize: normalFontSize,
-        bigFontSize: bigFontSize,
-      ),));
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => DetailedInformationPage(
+          smallFontSize: smallFontSize,
+          normalFontSize: normalFontSize,
+          bigFontSize: bigFontSize,
+        ),
+      ));
     }
 
     void _openQRCodeScanner() {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => QrCodeScanner(
-        smallFontSize: smallFontSize,
-        normalFontSize: normalFontSize,
-        bigFontSize: bigFontSize,
-      ),));
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => QrCodeScanner(
+          smallFontSize: smallFontSize,
+          normalFontSize: normalFontSize,
+          bigFontSize: bigFontSize,
+        ),
+      ));
     }
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Schnelldiagnose'),
-          actions: [
-            IconButton(icon: Icon(Icons.add_circle), onPressed: _openDetailedInformation)
-          ],
-        ),
-        body: ListView(
-          children: [
-            VisualInformation(
-              smallFontSize: smallFontSize,
-              normalFontSize: normalFontSize,
-              bigFontSize: bigFontSize,
-            ),
-            BuildButtonSection(containerColor),
-            BuildFirstInformationSection(
-              smallFontSize: smallFontSize,
-              normalFontSize: normalFontSize,
-              bigFontSize: bigFontSize,
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Schnelldiagnose'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add_circle), onPressed: _openDetailedInformation)
+        ],
+      ),
+      body: ListView(
+        children: [
+          VisualInformation(
+            smallFontSize: smallFontSize,
+            normalFontSize: normalFontSize,
+            bigFontSize: bigFontSize,
+          ),
+          BuildButtonSection(containerColor),
+          BuildFirstInformationSection(
+            smallFontSize: smallFontSize,
+            normalFontSize: normalFontSize,
+            bigFontSize: bigFontSize,
+          ),
+          PatientData(),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-      onPressed: _openQRCodeScanner,
-      tooltip: 'QR-Code-Scanner',
-      child: Icon(Icons.qr_code),
-    ),
+        onPressed: _openQRCodeScanner,
+        tooltip: 'QR-Code-Scanner',
+        child: Icon(Icons.qr_code),
+      ),
     );
   }
 }
 
+class PatientData extends StatefulWidget {
+  @override
+  _PatientDataState createState() => _PatientDataState();
+}
+
+class _PatientDataState extends State<PatientData> {
+  final _data_of_patient = <String>['Blutdruck', 'Puls', 'Atemfrequenz'];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Expanded(
+          child: SizedBox(
+              height: 200.0,
+              child: new ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: _data_of_patient.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 50,
+                      child: Center(
+                          child: Text('Entry ${_data_of_patient[index]}')),
+                    );
+                  }))),
+    ]);
+  }
+}
